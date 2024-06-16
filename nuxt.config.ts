2317f54390fce0e7cdea9776a45f82b75425212c
ilshaw <pe4enka519@gmail.com>
2024-06-16 05:31:32 +0300

    Feature: added auth middleware

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 export default defineNuxtConfig({
     dir: {
-        app: "./src"
+        app: "./src",
+        pages: "./src/pages",
+        middleware: "./src/middlewares"
     },
     nitro: {
         devProxy: {
             "/api": "http://localhost:8080"
         }
-    }
+    },
+    ssr: false
 });
---
+++ src/pages/user/profile.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+    definePageMeta({
+        middleware: "auth"
+    });
+</script>
+
+<template>
+    <span>
+        Profile
+    </span>
+</template>
+
+<style scoped lang="scss">
+
+</style>
