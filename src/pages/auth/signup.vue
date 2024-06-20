b8558d8070cab2f7903f2415a22932eb177ea74c
ilshaw <pe4enka519@gmail.com>
2024-06-20 05:22:47 +0300

    Refactoring: updated signup page

--- src/pages/auth/signup.vue
+++ src/pages/auth/signup.vue
@@ -1,13 +1,51 @@
 <script setup lang="ts">
+    import Control from "../../components/control.vue";
+    import Button from "../../components/button.vue";
+    import Footer from "../../components/footer.vue";
+    import Header from "../../components/header.vue";
+    import Input from "../../components/input.vue";
+    import Form from "../../components/form.vue";
+    import Page from "../../components/page.vue";
+    import Text from "../../components/text.vue";
+    import Main from "../../components/main.vue";
+    import Link from "../../components/link.vue";
 
+    const state = ref({
+        password: "",
+        email: ""
+    });
+
+    function submit(event: any) {
+        
+    }
 </script>
 
 <template>
-    <span>
-        Signup
-    </span>
+    <Page class="page-signup">
+        <Header class="header-signup">
+            Header
+        </Header>
+        <Main class="main-signup">
+            <Form class="form-signup" :state="state" @submit="(event) => submit(event)">
+                <Text>
+                    Registration
+                </Text>
+                <Control required name="email" label="Email">
+                    <Input type="email" v-model="state.email"/>
+                </Control>
+                <Control required name="password" label="Password">
+                    <Input type="password" v-model="state.password"/>
+                </Control>
+                <Button block type="submit">
+                    Signup
+                </Button>
+                <Text>
+                    Already have an account? Go <Link to="/auth/login">login</Link>.
+                </Text>
+            </Form>
+        </Main>
+        <Footer class="footer-signup">
+            Footer
+        </Footer>
+    </Page>
 </template>
-
-<style scoped lang="scss">
-
-</style>
