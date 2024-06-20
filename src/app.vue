593fb0a77290992a82ab81251ef8997b14ee7d72
ilshaw <pe4enka519@gmail.com>
2024-06-20 05:22:25 +0300

    Refactoring: updated app page

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,26 @@
 <template>
-    <NuxtLayout>
-        <NuxtPage/>
-    </NuxtLayout>
+    <Html class="html">
+        <Head>
+            <Title>
+                Application
+            </Title>
+        </Head>
+        <Body class="body">
+            <NuxtPage/>
+        </Body>
+    </Html>
 </template>
+
+<style lang="scss">
+    .html {
+        @apply flex flex-col justify-center items-center w-full min-w-full h-full min-h-full text-base;
+    }
+
+    .body {
+        @apply flex flex-col justify-center items-center w-full min-w-full h-full min-h-full text-base;
+    }
+
+    #__nuxt {
+        @apply flex flex-col justify-center items-center w-full min-w-full h-full min-h-full text-base;
+    }
+</style>
